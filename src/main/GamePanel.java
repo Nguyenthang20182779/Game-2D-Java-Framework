10b82186c891ee507de20691d3af0e8605e7409c
= <=>
2022-06-14 11:39:10 +0700

    4th commit

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -23,7 +23,9 @@ public class GamePanel extends JPanel implements Runnable {
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
     public SuperObject obj[] = new SuperObject[10];
-
+    Sound music = new Sound();
+    Sound se = new Sound();
+    public UI ui = new UI(this);
     //FPS
     int FPS = 60;
     //set default's player position
@@ -35,7 +37,6 @@ public class GamePanel extends JPanel implements Runnable {
     public final int maxWorldRow = 50;
     public final int worldWidth = titleSize * maxWorldCol; //48*50=2400
     public final int worldHeight = titleSize * maxWorldRow;
-
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
@@ -43,16 +44,14 @@ public class GamePanel extends JPanel implements Runnable {
         this.addKeyListener(keyH);
         this.setFocusable(true);
     }
-
     public void startGameThread(){
         gameThread = new Thread(this);
         gameThread.start();
     }
-
     public void setupGame(){
         aSetter.setObject();
+        playMusic(0);
     }
-
     // Delta method
     public void run(){
         double drawInterval = 1000000000 / FPS;  // 0.0166666667 seconds
@@ -61,13 +60,11 @@ public class GamePanel extends JPanel implements Runnable {
         long currentTime;
         long timer = 0;
         int drawCount = 0;
-
         while (gameThread != null){
             currentTime = System.nanoTime();
             delta += (currentTime - lastTime) / drawInterval;
             timer += (currentTime - lastTime);
             lastTime = currentTime;
-
             if(delta >= 1){
                 update();
                 repaint();
@@ -81,11 +78,9 @@ public class GamePanel extends JPanel implements Runnable {
             }
         }
     }
-
     public void update(){
         player.update();
     }
-
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
@@ -96,9 +91,9 @@ public class GamePanel extends JPanel implements Runnable {
             }
         }
         player.draw(g2);
+        ui.draw(g2);
         g2.dispose();
     }
-
     //Zoom in and zoom out function
    public void zoomInOut(int i){
         int oldWorldWidth = titleSize * maxWorldCol;  //2400
@@ -109,27 +104,31 @@ public class GamePanel extends JPanel implements Runnable {
         System.out.println("titleSize: " + titleSize);
         System.out.println("worldWidth: " + newWorldWidth);
         System.out.println("player.worldX: " + player.worldX);
-
         double newPlayerWorldX = player.worldX * multiplier;
         double newPlayerWorldY = player.worldY * multiplier;
         player.worldX = newPlayerWorldX;
         player.worldY = newPlayerWorldY;
     }
-
-
+    public void playMusic(int i){
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+    public void stopMusic(){
+        music.stop();
+    }
+    public void playSE(int i){
+        se.setFile(i);
+        se.play();
+    }
     // Sleep method
 /*    @Override
     public void run() {
-
         double drawInterval = 1000000000 / FPS; // 0.0166666667 seconds
         double nextDrawTime = System.nanoTime() + drawInterval;
-
         while (gameThread != null){
-
             update(); //1. Update: update information such as character positions
-
             repaint(); //2. Draw: draw the screen with the update information
-
             try {
                 double remainingTime = nextDrawTime - System.nanoTime();
                 remainingTime = remainingTime / 1000000;
@@ -141,13 +140,7 @@ public class GamePanel extends JPanel implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
     }
 */
-
-
-
-
-
 }
