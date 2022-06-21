28f061c0a5a13f1b09a0ff836388b43d43c9f3d5
= <=>
2022-06-21 19:20:47 +0700

    6th commit

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -17,32 +17,36 @@ public class GamePanel extends JPanel implements Runnable {
     public int maxScreenRow = 12;
     public int screenWidth = titleSize * maxScreenCol; // 768 pixels
     public int screenHeight = titleSize * maxScreenRow; // 576 pixels
-    KeyHandler keyH = new KeyHandler(this);
+    //World setting
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = titleSize * maxWorldCol; //48*50=2400
+    public final int worldHeight = titleSize * maxWorldRow;
+    //System
     Thread gameThread;
-    public Player player = new Player(this, keyH);
+    KeyHandler keyH = new KeyHandler(this);
     TileManager tileM = new TileManager(this);
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
-    public SuperObject obj[] = new SuperObject[10];
-    public Entity npc[] = new Entity[10];
     Sound music = new Sound();
     Sound se = new Sound();
     public UI ui = new UI(this);
+    //Entity and Object
+    public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
+    public Entity npc[] = new Entity[10];
     //FPS
     int FPS = 60;
     //set default's player position
     int playerX = 100;
     int playerY = 100;
     int playerSpeed = 4;
-    //World setting
-    public final int maxWorldCol = 50;
-    public final int maxWorldRow = 50;
-    public final int worldWidth = titleSize * maxWorldCol; //48*50=2400
-    public final int worldHeight = titleSize * maxWorldRow;
     //Game state
     public int gameState;
     public final int playState = 1;
     public final int pauseState = 2;
+    public final int dialougeState = 3;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
@@ -60,32 +64,6 @@ public class GamePanel extends JPanel implements Runnable {
         playMusic(0);
         stopMusic();
         gameState = playState;
-    }
-    // Delta method
-    public void run(){
-        double drawInterval = 1000000000 / FPS;  // 0.0166666667 seconds
-        double delta = 0;
-        long lastTime = System.nanoTime();
-        long currentTime;
-        long timer = 0;
-        int drawCount = 0;
-        while (gameThread != null){
-            currentTime = System.nanoTime();
-            delta += (currentTime - lastTime) / drawInterval;
-            timer += (currentTime - lastTime);
-            lastTime = currentTime;
-            if(delta >= 1){
-                update();
-                repaint();
-                delta--;
-                drawCount++;
-            }
-            if(timer >= 1000000000){
-                System.out.println("FPS: " + drawCount);
-                drawCount = 0;
-                timer = 0;
-            }
-        }
     }
     public void update(){
         if(gameState == playState){
@@ -164,6 +142,32 @@ public class GamePanel extends JPanel implements Runnable {
     public void playSE(int i){
         se.setFile(i);
         se.play();
+    }
+    // Delta method
+    public void run(){
+        double drawInterval = 1000000000 / FPS;  // 0.0166666667 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            if(timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
     }
     // Sleep method
 /*    @Override
--- src/main/UI.java
+++ src/main/UI.java
@@ -21,8 +21,8 @@ public class UI {
         this.gp = gp;
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         arial_80B = new Font("Arial", Font.BOLD, 80);
-        //OBJ_Key key = new OBJ_Key(gp);
-        //keyImage = key.image;
+        OBJ_Key key = new OBJ_Key(gp);
+        keyImage = key.image;
     }
     public void showMessage(String text){
         message = text;
@@ -39,60 +39,74 @@ public class UI {
         int x = gp.screenWidth/2 - length/2;
         return x;
     }
+    public void drawDialogueScreen(){
+        int x = gp.titleSize*2;
+        int y = gp.titleSize/2;
+        int width = gp.screenWidth - gp.titleSize*4;
+        int height = gp.screenHeight*5;
+        drawSubWindow(x, y, width, height);
+    }
+    public void drawSubWindow(int x, int y, int width, int height){
+        Color c = new Color(0, 0,0);
+        g2.setColor(c);
+        g2.fillRoundRect(x, y, width, height, 35, 35);
+    }
     public void draw(Graphics2D g2){
         this.g2 = g2;
         g2.setFont(arial_40);
         g2.setColor(Color.WHITE);
         if(gp.gameState == gp.playState){
+            if(gameFinished == true){
+                String text;
+                int textLength;
+                int x, y;
+                g2.setFont(arial_40);
+                g2.setColor(Color.WHITE);
+                text = "You found the treasure!";
+                textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+                x = gp.screenWidth/2 - textLength/2;
+                y = gp.screenHeight/2 - (gp.titleSize*3);
+                g2.drawString(text, x, y);
+                text = "Your time is : " + dFomat.format(playTime) + "!";
+                textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+                x = gp.screenWidth/2 - textLength/2;
+                y = gp.screenHeight/2 + (gp.titleSize*4);
+                g2.drawString(text, x, y);
 
+                g2.setFont(arial_80B);
+                g2.setColor(Color.YELLOW);
+                text = "Congratulation!";
+                textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+                x = gp.screenWidth/2 - textLength/2;
+                y = gp.screenHeight/2 + (gp.titleSize*2);
+                g2.drawString(text, x, y);
+                gp.gameThread = null;
+            }
+            else {
+                g2.setFont(arial_40);
+                g2.setColor(Color.WHITE);
+                g2.drawImage(keyImage, gp.titleSize/2, gp.titleSize/2, gp.titleSize, gp.titleSize, null);
+                g2.drawString(" x = " + gp.player.hasKey, 74, 65);
+                //Message
+                if(messageOn == true){
+                    g2.setFont(g2.getFont().deriveFont(30F));
+                    g2.drawString(message, gp.titleSize/2, gp.titleSize*5);
+                    messageCounter++;
+                    if(messageCounter > 120){
+                        messageCounter = 0;
+                        messageOn = false;
+                    }
+                }
+                //Time
+                playTime += (double) 1/60;
+                g2.drawString("Time: " + dFomat.format(playTime), gp.titleSize*11, 65);
+            }
         }
         if(gp.gameState == gp.pauseState){
             drawPauseState();
         }
-        if(gameFinished == true){
-            String text;
-            int textLength;
-            int x, y;
-            g2.setFont(arial_40);
-            g2.setColor(Color.WHITE);
-            text = "You found the treasure!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 - (gp.titleSize*3);
-            g2.drawString(text, x, y);
-            text = "Your time is : " + dFomat.format(playTime) + "!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + (gp.titleSize*4);
-            g2.drawString(text, x, y);
-
-            g2.setFont(arial_80B);
-            g2.setColor(Color.YELLOW);
-            text = "Congratulation!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + (gp.titleSize*2);
-            g2.drawString(text, x, y);
-            gp.gameThread = null;
-        }
-        else {
-            g2.setFont(arial_40);
-            g2.setColor(Color.WHITE);
-            g2.drawImage(keyImage, gp.titleSize/2, gp.titleSize/2, gp.titleSize, gp.titleSize, null);
-            g2.drawString(" x = " + gp.player.hasKey, 74, 65);
-            //Message
-            if(messageOn == true){
-                g2.setFont(g2.getFont().deriveFont(30F));
-                g2.drawString(message, gp.titleSize/2, gp.titleSize*5);
-                messageCounter++;
-                if(messageCounter > 120){
-                    messageCounter = 0;
-                    messageOn = false;
-                }
-            }
-            //Time
-            playTime += (double) 1/60;
-            g2.drawString("Time: " + dFomat.format(playTime), gp.titleSize*11, 65);
+        if(gp.gameState == gp.dialougeState){
+            drawDialogueScreen();
         }
     }
 }
